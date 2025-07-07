d4ec3320ca834638800bcc2454166aed05e69020
Finn Terdal <finnterdal@gmail.com>
2025-07-07 03:17:03 -0400

    seed users

--- database/seed.js
+++ database/seed.js
@@ -1,9 +1,16 @@
 const db = require("./db");
-const { Duck } = require("./index");
+const { Duck, User } = require("./index");
 
 const seed = async () => {
   db.logging = false;
   await db.sync({ force: true }); // Drop and recreate tables
+
+  const users = await User.bulkCreate([
+    { username: "admin", passwordHash: User.hashPassword("password") },
+    { username: "user1", passwordHash: User.hashPassword("password") },
+    { username: "user2", passwordHash: User.hashPassword("password") },
+  ]);
+
   const ducks = await Duck.bulkCreate([
     { name: "James Pond" },
     { name: "Quakie Chan" },
